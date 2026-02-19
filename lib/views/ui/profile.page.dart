5e6af9a797755bbf6191bc0ee3162e75266dae2e
LeFab70 <kouonang2002@gmail.com>
2026-02-18 23:00:10 -0400

    profil ok

--- lib/views/ui/profile.page.dart
+++ lib/views/ui/profile.page.dart
@@ -1,3 +1,5 @@
+import 'package:test1_appgardienbut_fabrice/views/shared/colors/colors.app.dart';
+
 import '../shared/styles/app.style.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +14,39 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('profile page', style: appStyle(40, Colors.black, .bold)),
+      appBar: AppBar(
+        title: Text('Paramètres', style: appStyle(20, Colors.black, FontWeight.bold)),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              child: ListTile(
+                leading: const Icon(Icons.dark_mode_outlined),
+                title: Text('Mode Sombre', style: appStyle(18, Colors.black, FontWeight.w500)),
+                trailing: Switch(
+                  value: false,
+                  onChanged: (value) {},
+                  hoverColor: AppColors.textColor,
+                ),
+              ),
+            ),
+            const SizedBox(height: 60,),
+            Expanded(
+              child: Column(
+                children: [
+                  Text("Author: Fabrice Kouonang",style: appStyle(20, AppColors.textColor, FontWeight.bold)),
+                  Text("Version 1.0.0\nCCNB-2026", style: appStyle(14, Colors.grey, FontWeight.normal)),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
